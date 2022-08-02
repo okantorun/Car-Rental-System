8929d3b4c086590e5198be88669247a508be49ab
Okan Torun <torun.okn@gmail.com>
2022-08-03 02:12:40 +0300

    tablolar ve appsettings oluşturuldu

---
+++ Core/Entities/Concrate/OperationClaim.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities.Concrate
+{
+    public class OperationClaim : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Core/Entities/Concrate/UserOperationClaim.cs
@@ -0,0 +1,11 @@
+﻿using Entities.Abstract;
+
+namespace Core.Entities.Concrate
+{
+    public class UserOperationClaim : IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int OperationClaimId { get; set; }
+    }
+}
