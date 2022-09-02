067216e4aa77b38aa49a125f517df0860e7c36c7
Okan Torun <torun.okn@gmail.com>
2022-09-03 01:49:54 +0300

    UseCors is added for backend

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -38,6 +38,7 @@ namespace WebAPI
         {
             services.AddControllers();
 
+            services.AddCors();
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -68,6 +69,8 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
